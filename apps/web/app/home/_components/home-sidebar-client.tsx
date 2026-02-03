687f06c4567282e3819761c3988433b32951d566
deepalixotiv <deepali.xotiv@gmail.com>
2026-02-03 13:22:34 +0530

    side-bar

--- apps/web/app/home/_components/home-sidebar-client.tsx
+++ apps/web/app/home/_components/home-sidebar-client.tsx
@@ -25,19 +25,7 @@ export function HomeSidebarClient(props: { user: JwtPayload }) {
 
   const navConfig = useMemo(() => {
     // Basic routes that always exist
-    const baseRoutes = [
-      {
-        label: 'common:routes.application',
-        children: [
-          {
-            label: 'common:routes.home',
-            path: pathsConfig.app.home,
-            Icon: <Activity className="h-4 w-4" />,
-            end: true,
-          },
-        ],
-      },
-    ];
+    const baseRoutes: any[] = [];
 
     const settingsRoutes = [
       {
@@ -60,33 +48,45 @@ export function HomeSidebarClient(props: { user: JwtPayload }) {
         ...baseRoutes,
         ...(salesItems.length > 0
           ? [
-              {
-                label: 'Sales',
-                children: salesItems.map((item) => {
+            {
+              label: 'Sales',
+              children: [
+                {
+                  label: 'common:routes.dashboard',
+                  path: pathsConfig.app.home,
+                  Icon: <Activity className="h-4 w-4" />,
+                  end: true,
+                },
+                ...salesItems.map((item) => {
                   const IconComponent = item.Icon;
                   return {
                     ...item,
                     Icon: <IconComponent className="h-4 w-4" />,
                   };
                 }),
-              },
-            ]
+              ],
+            },
+          ]
           : []),
-        ...(teamItems.length > 0
-          ? [
-              {
-                label: 'Team',
-                children: teamItems.map((item) => {
-                  const IconComponent = item.Icon;
-                  return {
-                    ...item,
-                    Icon: <IconComponent className="h-4 w-4" />,
-                  };
-                }),
-              },
-            ]
-          : []),
-        ...settingsRoutes,
+        {
+          label: 'common:routes.settings',
+          children: [
+            {
+              label: 'common:routes.profile',
+              path: pathsConfig.app.profileSettings,
+              Icon: <Settings className="h-4 w-4" />,
+            },
+            ...(teamItems.length > 0
+              ? teamItems.map((item) => {
+                const IconComponent = item.Icon;
+                return {
+                  ...item,
+                  Icon: <IconComponent className="h-4 w-4" />,
+                };
+              })
+              : []),
+          ],
+        },
       ];
     }
 
@@ -97,33 +97,45 @@ export function HomeSidebarClient(props: { user: JwtPayload }) {
       ...baseRoutes,
       ...(salesItems.length > 0
         ? [
-            {
-              label: 'Sales',
-              children: salesItems.map((item) => {
+          {
+            label: 'Sales',
+            children: [
+              {
+                label: 'common:routes.dashboard',
+                path: pathsConfig.app.home,
+                Icon: <Activity className="h-4 w-4" />,
+                end: true,
+              },
+              ...salesItems.map((item) => {
                 const IconComponent = item.Icon;
                 return {
                   ...item,
                   Icon: <IconComponent className="h-4 w-4" />,
                 };
               }),
-            },
-          ]
+            ],
+          },
+        ]
         : []),
-      ...(teamItems.length > 0
-        ? [
-            {
-              label: 'Team',
-              children: teamItems.map((item) => {
-                const IconComponent = item.Icon;
-                return {
-                  ...item,
-                  Icon: <IconComponent className="h-4 w-4" />,
-                };
-              }),
-            },
-          ]
-        : []),
-      ...settingsRoutes,
+      {
+        label: 'common:routes.settings',
+        children: [
+          {
+            label: 'common:routes.profile',
+            path: pathsConfig.app.profileSettings,
+            Icon: <Settings className="h-4 w-4" />,
+          },
+          ...(teamItems.length > 0
+            ? teamItems.map((item) => {
+              const IconComponent = item.Icon;
+              return {
+                ...item,
+                Icon: <IconComponent className="h-4 w-4" />,
+              };
+            })
+            : []),
+        ],
+      },
     ];
   }, [permissionNavConfig, canAccess]);
 
