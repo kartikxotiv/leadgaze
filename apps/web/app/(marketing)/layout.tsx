e2a8c022f491cd95dddad9c290c3a6e5c802d8dd
Kartik Gupta <kartik.gupta@xotiv.com>
2026-06-10 18:11:51 +0530

    chore: adjust session logout logic, and redirect marketing layout to sign-in

--- apps/web/app/(marketing)/layout.tsx
+++ apps/web/app/(marketing)/layout.tsx
@@ -1,29 +1,29 @@
 import { redirect } from 'next/navigation';
 
-import { getSupabaseServerClient } from '@kit/supabase/server-client';
+// import { getSupabaseServerClient } from '@kit/supabase/server-client';
 
-import { SiteFooter } from '~/(marketing)/_components/site-footer';
-import { SiteHeader } from '~/(marketing)/_components/site-header';
-import { withI18n } from '~/lib/i18n/with-i18n';
+// import { SiteFooter } from '~/(marketing)/_components/site-footer';
+// import { SiteHeader } from '~/(marketing)/_components/site-header';
+// import { withI18n } from '~/lib/i18n/with-i18n';
 
-async function SiteLayout(props: React.PropsWithChildren) {
-  const client = getSupabaseServerClient();
+// async function SiteLayout(props: React.PropsWithChildren) {
+//   const client = getSupabaseServerClient();
 
-  const { data } = await client.auth.getClaims();
+//   const { data } = await client.auth.getClaims();
 
-  return (
-    <div className={'flex min-h-[100vh] flex-col'}>
-      <SiteHeader user={data?.claims} />
+//   return (
+//     <div className={'flex min-h-[100vh] flex-col'}>
+//       <SiteHeader user={data?.claims} />
 
-      {props.children}
+//       {props.children}
 
-      <SiteFooter />
-    </div>
-  );
-}
-
-export default withI18n(SiteLayout);
-
-// export default function SalesPage() {
-//   redirect('/auth/sign-in');
+//       <SiteFooter />
+//     </div>
+//   );
 // }
+
+// export default withI18n(SiteLayout);
+
+export default function SalesPage() {
+  redirect('/auth/sign-in');
+}
